c8d91eebbc6295af4a46ccd1140dbdbc0a5d02ea
ginomiguel91 <ginomiguel91@gmail.com>
2023-08-14 15:05:22 -0400

    Add pipes project

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,8 +3,11 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+
 
 @NgModule({
-  exports: [ButtonModule, CardModule, MenubarModule, FieldsetModule],
+  exports: [ButtonModule, CardModule, MenubarModule, FieldsetModule,ToolbarModule,TableModule],
 })
 export class PrimeNgModule {}
--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
-  styleUrls: ['./ordenar.component.css']
+  styleUrls: ['./ordenar.component.css'],
 })
 export class OrdenarComponent {
+  enMayuscula: boolean = false;
+  ordenarPor: string = '';
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul,
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde,
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo,
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde,
+    },
+  ];
 
+  aMayuscula(): void {
+    this.enMayuscula = !this.enMayuscula;
+  }
+
+  cambiarOrden(valor: string) {
+    this.ordenarPor = valor;
+    console.log(valor);
+  }
 }
